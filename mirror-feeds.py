5519d46b7ff748ed7a4ff09319356ec868850486
Binoj David <dbinoj@gmail.com>
2016-11-10 16:04:28 +0530

    v1

--- mirror-feeds.py
+++ mirror-feeds.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 """
+Thanks to Miroslav Stampar (@stamparm) for the software "maltrail"
+This script has code from stamparm/maltrail's update logic which is used to download the feeds.
 Copyright (c) 2016 Binoj D (@dbinoj)
 See the file 'LICENSE' for copying permission
 """
